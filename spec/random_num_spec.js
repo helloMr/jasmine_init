eb85f24f7134ea72bb7598e9cdad1b3a878a5df2
aaa <1143581723@qq.com>
2013-09-25 14:57:51 +0800

    !test random number

---
+++ spec/random_num_spec.js
@@ -0,0 +1,111 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: aaa
+ * Date: 13-9-24
+ * Time: 下午4:15
+ * To change this template use File | Settings | File Templates.
+ */
+
+describe("random_num.js", function () {
+
+    it("Is four numbers?", function () {
+        var nums = createRandomNum();
+
+        var N = 0;
+        for (i = 0; i < 4; i++) {
+            if (nums[i] !== "") {
+                N++;
+            }
+        }
+        expect(
+
+            N
+
+        ).toBe(4);
+    });
+
+    it("Is random number?", function () {
+        var tmpNum;
+        var frequency = new Array();
+        var sum = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        var rightP=0;
+        for ( i = 0; i < 8; i+=4) {
+            tmpNum = createRandomNum();
+            frequency[i]=tmpNum[0];
+            frequency[i+1]=tmpNum[1];
+            frequency[i+2]=tmpNum[2];
+            frequency[i+3]=tmpNum[3];
+
+        }
+
+//        for (i = 0; i < 100; i++)
+//        {
+//
+//                if (frequency[i] === 0) {
+//                    sum[0]++ ;
+//                }
+//                if (frequency[i]=== 1) {
+//                    sum[1]++ ;
+//                }
+//                if (frequency[i] === 2) {
+//                    sum[2]++ ;
+//                }
+//                if (frequency[i] === 3) {
+//                    sum[3]++  ;
+//                }
+//                if (frequency[i] === 4) {
+//                    sum[4]++  ;
+//                }
+//                if (frequency[i] === 5) {
+//                    sum[5]++  ;
+//                }
+//                if (frequency[i] === 6) {
+//                    sum[6]++  ;
+//                }
+//                if (frequency[i] === 7) {
+//                    sum[7]++   ;
+//                }
+//                if (frequency[i] === 8) {
+//                    sum[8]++   ;
+//                }
+//                if (frequency[i] === 9) {
+//                    sum[9]++   ;
+//                }
+//
+//        }
+
+//        for(i=0;i<10;i++)
+//        {
+//            var per=sum[i]/100;
+//            if(per>=0.07&&per<=0.13)
+//            {
+//                rightP++;
+//            }
+//        }
+
+//       expect(rightP).toBe(10);
+
+
+
+    });
+
+    it("Is different number?", function () {
+//        var cnums = createRandomNum();
+//        var count_diff=0;
+//        for(var i=0;i<4;i++)
+//        {
+//            for(var j=i;j<4;j++)
+//            {
+//                if(cnums[i]===cnums[j])
+//                    count_diff++;
+//            }
+//        }
+//        expect(
+//
+//            count_diff
+//
+//        ).toBe(0);
+
+    });
+
+});
---
+++ src/random_num.js
@@ -0,0 +1,81 @@
+/*window.onload=function(){
+    var n=na=nb=nc='';
+    n=parseInt(Math.random()*10);
+    na=parseInt(Math.random()*10);
+    nb=parseInt(Math.random()*10);
+    nc=parseInt(Math.random()*10); */
+    //na=parseInt(Math.random()*(20-11+1) + 11);
+    //nb=parseInt(Math.random()*100+1);
+   // nc=parseInt(Math.random()*(100-51+1) + 51);
+
+//    var o=document.getElementsByTagName('input');
+//    o[0].value=n
+//    o[1].value=na;
+//    o[2].value=nb;
+//    o[3].value=nc;
+
+    //11-20: <input type="text" /><br />
+   // 1-100: <input type="text" /><br />
+    //51-100: <input type="text" /><br />
+
+//} // shawl.qiu script
+
+
+function createRandomNum() {
+
+    var randomNum = new Array()
+    var tmp=-1;
+    var repeat=false;
+    for (i=0;i<4;i++)
+    {
+        repeat=0;
+        tmp=parseInt(Math.random() * 10);
+        for(j=0;j<i;j++)
+        {
+            if(tmp==randomNum[j])
+            {
+                repeat=true;
+                break;
+            }
+        }
+        if(!repeat)
+        {
+            randomNum[i]=tmp;
+        }
+        else
+        {
+            i=i-1;//若重复，循环变量i-1
+        }
+    }
+
+    //显示随机数
+//    var o=document.getElementsByTagName('input');
+//    for(i=0;i<4;i++)
+//    {
+//        o[i].value=randomNum[i];
+//    }
+   return  randomNum;
+
+
+
+
+
+//    var n1 = n2 = n3 = n4 = '';
+//    n1 = parseInt(Math.random() * 10);
+//    n2 = parseInt(Math.random() * 10);
+//    n3 = parseInt(Math.random() * 10);
+//    n4 = parseInt(Math.random() * 10);
+//
+//    var randomNum = new Array()
+//    randomNum[0] = n1
+//    randomNum[1] = n2
+//    randomNum[2] = n3
+//    randomNum[3] = n4
+//
+//
+//    return randomNum;
+}
+
+
+
+
